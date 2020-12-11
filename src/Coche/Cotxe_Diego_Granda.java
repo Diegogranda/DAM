43b8ebe56a8dce2065a62eb03a22f3400afba959
dgranda <dgranda@nigul.cide.es>
2020-12-11 17:33:46 +0100

    ejercicio coche 1 finalizado

--- src/Coche/Cotxe_Diego_Granda.java
+++ src/Coche/Cotxe_Diego_Granda.java
@@ -5,27 +5,49 @@ Date: 09/12/2020
 */
 
 public class Cotxe_Diego_Granda extends CotxeAbstracte implements InterfaceCotxe{
-
+private  EstatsMotorCotxe Estado=EstatsMotorCotxe.Aturat;
+private int revoluciones=0;
     public Cotxe_Diego_Granda(String marca, String model, TipusCanvi tipuscanvi) {
         super(marca, model, tipuscanvi);
     }
 
     @Override
     public void arrancarMotor() throws Exception {
+        if (Estado.equals(EstatsMotorCotxe.Aturat)) {
+            Estado=EstatsMotorCotxe.EnMarxa;
+        }else{
+            throw new Exception("El coche ya está en marcha");
+        }
     }
 
     @Override
     public EstatsMotorCotxe comprovaMotor() {
-        return null;
+        if (Estado.equals(EstatsMotorCotxe.EnMarxa)){
+            System.out.println("El motor está en marcha");
+        }
+        else{
+            System.out.println("El motor está apagado");
+        }
+        return Estado;
     }
 
     @Override
     public int getRevolucions() {
-        return 0;
+        if (Estado.equals(EstatsMotorCotxe.EnMarxa)){
+            revoluciones= (int) (Math.random()*6500+1);
+        }
+        else {
+            revoluciones=0;
+        }
+        return revoluciones;
     }
 
     @Override
     public void aturarMotor() throws Exception {
-
+        if (Estado.equals(EstatsMotorCotxe.EnMarxa)) {
+            Estado=EstatsMotorCotxe.Aturat;
+        }else{
+            throw new Exception("El coche ya está aturat");
+        }
     }
 }
