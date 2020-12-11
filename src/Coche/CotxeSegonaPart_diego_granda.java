5e38d730ed0b87ba70dab3089d74f964578efc92
dgranda <dgranda@nigul.cide.es>
2020-12-11 19:00:57 +0100

    clase 2 creacion

---
+++ src/Coche/CotxeSegonaPart_diego_granda.java
@@ -0,0 +1,14 @@
+package Coche;/*
+Coche
+Created by: Diego Granda
+Date: 11/12/2020
+*/
+
+public class CotxeSegonaPart_diego_granda extends Cotxe_Diego_Granda {
+    public CotxeSegonaPart_diego_granda(String marca, String model, TipusCanvi tipuscanvi) {
+        super(marca, model, tipuscanvi);
+    }
+
+
+
+}
